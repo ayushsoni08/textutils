f1b532d53210f474555ebb8d4e5650cb09aba350
Ayush <ayushsoni0718@gmail.com>
2022-09-04 12:38:45 +0530

    Updates

---
+++ precache-manifest.a84e2441fc897a1dbaf626e739e5fffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1371dfe5f929c4d4463f",
+    "url": "/textutils/static/css/main.184e62cd.chunk.css"
+  },
+  {
+    "revision": "1371dfe5f929c4d4463f",
+    "url": "/textutils/static/js/main.8ed81bac.chunk.js"
+  },
+  {
+    "revision": "ca4ec9a452fa551cdfa8",
+    "url": "/textutils/static/js/runtime~main.9ba8c014.js"
+  },
+  {
+    "revision": "d7552a1f1f052f900238",
+    "url": "/textutils/static/js/2.265c2a91.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/textutils/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "e0584a50a33badabcaa65642cc1d8981",
+    "url": "/textutils/index.html"
+  }
+];
